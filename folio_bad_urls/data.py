11297f2ce0f4acb2d390dc070b66811ff8a05cb7
Maccabee Levine <msl321@lehigh.edu>
2022-04-29 17:11:43 -0400

    Remove static typing

--- folio_bad_urls/data.py
+++ folio_bad_urls/data.py
@@ -1,18 +1,14 @@
 class ElectronicRecord:
     """ A record with a URL. """
 
-    instance_hrid: str|None
-    url: str
+    instance_hrid = None
+    url = None
 
     def __repr__(self):
         return str(self.__dict__)
 
 class TestResult:
     """ The result of testing a URL from a record. """ 
-
-    instance_hrid: str|None
-    url: str
-    status_code: int
 
     def __init__(self, instance_hrid, url, status_code):
         self.instance_hrid = instance_hrid
